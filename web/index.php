d9379e87b36d913c26273886735f15d0c79176a8
louise-saguin <louise.saguin@hetic.net>
2017-07-16 17:16:36 +0200

    header

--- web/index.php
+++ web/index.php
@@ -19,12 +19,33 @@ $app->get('/', function() use ($app)
 })
 ->bind('home');
 
+// Produits
+$app->get('/produits', function() use ($app)
+{
+	return $app['twig']->render('pages/produits.twig');
+})
+->bind('produits');
+
+// Plan
+$app->get('/plan', function() use ($app)
+{
+	return $app['twig']->render('pages/plan.twig');
+})
+->bind('plan');
+
 // Docs
 $app->get('/documents-utiles', function() use ($app)
 {
 	return $app['twig']->render('pages/docs.twig');
 })
 ->bind('docs');
+
+// Contact
+$app->get('/contact', function() use ($app)
+{
+	return $app['twig']->render('pages/contact.twig');
+})
+->bind('contact');
 
 // Run Silex
 $app->run();
