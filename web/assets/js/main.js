a2781fb4cef72888b8ab2f01b2a51eeb40d17706
louise-saguin <louise.saguin@hetic.net>
2017-07-19 18:06:36 +0200

    details

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -2,71 +2,101 @@ var slider_img = document.querySelectorAll('.slider-img > img')
 
 if(slider_img.length != 0)
 {
-	var i = slider_img.length - 1
+    var i = slider_img.length - 1
 
-	var interval = setInterval(slide, 3000)
+    var interval = setInterval(slide, 3000)
 
-	function slide()
-	{
-		if(i == 0)
-		{
-			slider_img[i].style.opacity = '0'
-			resetSlides()
-			i = slider_img.length - 1
-		}
-		else
-		{
-			slider_img[i].style.opacity = '0'
-			i--
-		}
-	}
+    function slide()
+    {
+        if(i == 0)
+        {
+            slider_img[i].style.opacity = '0'
+            resetSlides()
+            i = slider_img.length - 1
+        }
+        else
+        {
+            slider_img[i].style.opacity = '0'
+            i--
+        }
+    }
 
-	var left = document.querySelector('.left-control');
-	var right = document.querySelector('.right-control');
+    var left = document.querySelector('.left-control');
+    var right = document.querySelector('.right-control');
 
-	left.addEventListener('click', function(e) {
-		clearInterval(interval);
-		interval = setInterval(slide, 3000)
-		if(i == slider_img.length - 1)
-		{
-			clearSlides()
-			i = 0
-			slider_img[i].style.opacity = '1'
-		}
-		else
-		{
-			slider_img[i+1].style.opacity = '1'
-			i++
-		}
-	})
-	right.addEventListener('click', function(e) {
-		clearInterval(interval);
-		interval = setInterval(slide, 3000)
-		if(i == 0)
-		{
-			i = slider_img.length - 1
-			resetSlides()
-		}
-		else
-		{
-			slider_img[i].style.opacity = '0'
-			i--
-		}
-	})
+    left.addEventListener('click', function(e) {
+        clearInterval(interval);
+        interval = setInterval(slide, 3000)
+        if(i == slider_img.length - 1)
+        {
+            clearSlides()
+            i = 0
+            slider_img[i].style.opacity = '1'
+        }
+        else
+        {
+            slider_img[i+1].style.opacity = '1'
+            i++
+        }
+    })
+    right.addEventListener('click', function(e) {
+        clearInterval(interval);
+        interval = setInterval(slide, 3000)
+        if(i == 0)
+        {
+            i = slider_img.length - 1
+            resetSlides()
+        }
+        else
+        {
+            slider_img[i].style.opacity = '0'
+            i--
+        }
+    })
 
-	function resetSlides()
-	{
-		for(var i = 0; i < slider_img.length; i++)
-		{
-			slider_img[i].style.opacity = '1'
-		}
-	}
+    function resetSlides()
+    {
+        for(var i = 0; i < slider_img.length; i++)
+        {
+            slider_img[i].style.opacity = '1'
+        }
+    }
 
-	function clearSlides()
-	{
-		for(var i = 0; i < slider_img.length; i++)
-		{
-			slider_img[i].style.opacity = '0'
-		}
-	}
+    function clearSlides()
+    {
+        for(var i = 0; i < slider_img.length; i++)
+        {
+            slider_img[i].style.opacity = '0'
+        }
+    }
+}
+
+//CONTACT
+
+var inputs = document.querySelectorAll('.inputs > input');
+var textarea = document.querySelector('.inputs > textarea');
+var labels = document.querySelectorAll('.labels');
+
+for(var i=0; i<inputs.length; i++){
+
+    inputs[i].addEventListener('focus', function(e){
+        resetLabels();
+        labels[this.dataset.index].style.color = 'red'
+        labels[this.dataset.index].style.fontWeight ='bold'
+    })
+}
+
+textarea.addEventListener('focus', function(){
+    resetLabels();
+    labels[this.dataset.index].style.color = 'red'
+    labels[this.dataset.index].style.fontWeight ='bold'
+})
+
+function resetLabels()
+{
+    for(var i = 0; i < labels.length; i++)
+    {
+        labels[i].style.color = '#2e3689'
+        labels[i].style.fontWeight = 'normal'
+    }
 }
